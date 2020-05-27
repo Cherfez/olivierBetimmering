612005b58d3fe5e84a8c4516576d5d5cd9c621b5
Cherfez <chervasev.leeuwen@gmail.com>
2020-05-27 13:00:42 +0200

    basic layout of interieur-page

--- dist/main.js
+++ dist/main.js
@@ -5,3 +5,27 @@ let navBarToggle = document.getElementById("js-navbar-toggle");
 navBarToggle.addEventListener("click", function () {
   mainNav.classList.toggle("active");
 });
+
+// modal on img
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var x = document.getElementsByClassName("interieur-fotos");
+var img = document.getElementsByClassName("pic");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  console.log(this.src);
+  captionText.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
